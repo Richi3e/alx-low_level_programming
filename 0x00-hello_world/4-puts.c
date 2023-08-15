8ceb944af9882cc8cda845551dd17106797d71f3
Morakinyo Richard <richard.morakinyo@stu.cu.edu.ng>
2023-08-14 20:26:52 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - prints "Programming is like building a multilingual puzzle
-*return: always 0 (success)
+*main - Entry point
+*Return: always 0 (success)
 */
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
