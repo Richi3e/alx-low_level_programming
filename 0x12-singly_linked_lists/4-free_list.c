d5c1edca21e98cff072d5410a9e31c7a9df17d34
Richi3e <richardnifemi123@gmail.com>
2023-09-27 06:28:08 -0700

    ng Directory:

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: list_t list to be freed
+ */
+
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		list_t *temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
